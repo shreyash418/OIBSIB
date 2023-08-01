f817a24ea757ddecb6367d3efc731a5ab8a7a2f2
shreyash shukla <98917970+shreyash418@users.noreply.github.com>
2023-08-01 13:38:14 +0530

    Add files via upload

---
+++ OIBSIP-TASK-1 (Landing Page)/main2.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("#demo2").owlCarousel({
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true
+        },
+        600: {
+          items: 1,
+          nav: false
+        },
+        1000: {
+          items: 2,
+          nav: true,
+          loop: false,
+          margin: 20
+        }
+      }
+    });
+  });
+  
